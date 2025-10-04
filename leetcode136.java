5af6ba874b5502ffdba6632cb4374c56816910c1
Ranjitha2710 <ranjithasangamss@gmail.com>
2025-10-04 21:26:38 +0530

    leetcode 136

---
+++ leetcode136.java
@@ -0,0 +1,15 @@
+
+// DATE : 01-OCT-2025
+// LEVEL : Easy
+// TOPIC : Bit Manipulation(XOR)
+
+
+class Solution {
+    public int singleNumber(int[] nums) {
+        int res=0;
+        for(int i=0;i<nums.length;i++){
+            res^=nums[i];
+        }
+        return res;   
+    }
+}
