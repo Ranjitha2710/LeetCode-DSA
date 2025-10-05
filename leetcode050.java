e0ee948ce422fe5cc5b691e7b9ae9a04a98dec3e
Ranjitha2710 <ranjithasangamss@gmail.com>
2025-10-05 21:19:06 +0530

    leetcode50

---
+++ leetcode050.java
@@ -0,0 +1,23 @@
+
+// DATE : 05-OCT-2025
+// LEVEL : Medium
+// TOPIC : Bit Manipulation
+
+class Solution {
+    public double myPow(double x, int n) {
+        double ans=1;
+        long power=n;
+        if(power<0){
+            x=1/x;
+            power=-power;
+        }
+        while(power>0){
+            if((power & 1)!=0){
+                ans=ans*x;
+            }
+            x=x*x;
+            power=power>>1;
+        }
+        return ans;
+    }
+}
