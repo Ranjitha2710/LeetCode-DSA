2a2880b259a541e4ad8377f0adb3e51242e15f40
Ranjitha2710 <ranjithasangamss@gmail.com>
2025-10-08 13:56:28 +0530

    oct 08

---
+++ leetcode242.java
@@ -0,0 +1,24 @@
+
+DATE : 08-OCT-2025
+LEVEL : Easy
+TOPIC : Strings
+
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length()){
+            return false;
+        }
+        int arr[]=new int[26];
+        for(int i=0;i<s.length();i++){
+            arr[s.charAt(i)-'a']++;
+            arr[t.charAt(i)-'a']--;
+        }
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]!=0){
+                return false;
+            }
+        }
+        return true;
+        
+    }
+}
