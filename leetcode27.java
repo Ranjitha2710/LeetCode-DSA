0c7fcc90a0c1dcfaa6220fe447420ca6826b55d9
Ranjitha2710 <ranjithasangamss@gmail.com>
2025-10-14 18:05:19 +0530

    oct 14

---
+++ leetcode27.java
@@ -0,0 +1,18 @@
+
+DATE : 14-OCT-2025
+LEVEL : Easy
+TOPIC : Arrays
+
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int k=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=val){
+                nums[k]=nums[i];
+                k++;
+            }
+        }
+        return k;
+        
+    }
+}
