6f5e598086e40234e0362d6815efbdc8b43c7b63
Ranjitha2710 <ranjithasangamss@gmail.com>
2025-10-19 18:41:31 +0530

    oct 19

---
+++ leecode09.java
@@ -0,0 +1,19 @@
+
+DATE : 19-OCT-2025
+LEVEL : easy
+TOPIC : Math
+
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0 || (x % 10==0 && x!=0)){
+            return false;
+        }
+        int reverseHalf=0;
+        while(x>reverseHalf){
+            reverseHalf=reverseHalf*10+x%10;
+            x/=10;
+        }
+        return x==reverseHalf || x==reverseHalf/10;
+        
+    }
+}
