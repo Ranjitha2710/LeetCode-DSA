dfd05a1e46d41f3e0549a67e56d98cf9d58ac69b
Ranjitha2710 <ranjithasangamss@gmail.com>
2025-10-22 09:34:52 +0530

    oct 22

---
+++ leetcode75.java
@@ -0,0 +1,30 @@
+
+DATE : 22-OCT-2025
+LEVEL : Medium
+TOPIC : 3-Pointer Approach 
+
+class Solution {
+    public void sortColors(int[] nums) {
+        int start=0;
+        int end=nums.length-1;
+        int mid=0;
+        while(mid<=end){
+            if(nums[mid]==0){
+                int temp=nums[mid];
+                nums[mid]=nums[start];
+                nums[start]=temp;
+                mid++;
+                start++;
+            }else if(nums[mid]==1){
+                mid++;
+            }else{
+                int temp=nums[mid];
+                nums[mid]=nums[end];
+                nums[end]=temp;
+                end--;
+            }
+        }
+        
+        
+    }
+}
