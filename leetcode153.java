95bcf1ceffaa5a386096b094e8077ff17205bd36
Ranjitha2710 <ranjithasangamss@gmail.com>
2025-10-23 16:29:18 +0530

    oct 23

---
+++ leetcode153.java
@@ -0,0 +1,22 @@
+
+DATE : 23-OCT-2025
+LEVEL : Medium
+TOPIC : Binary Search
+
+class Solution {
+    public int findMin(int[] nums) {
+        int start=0;
+        int end=nums.length-1;
+        while(start<end){
+            int mid=start+(end-start)/2;
+            if(nums[mid]>nums[end]){
+                start=mid+1;
+            }else{
+                end=mid;
+            }
+        }
+        return nums[start];
+        //return nums[end];
+        
+    }
+}
