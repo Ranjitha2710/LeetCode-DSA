d4bfe03308c89a30055dfb1cca22f091cb45d08a
Ranjitha2710 <ranjithasangamss@gmail.com>
2025-10-29 16:27:20 +0530

    oct 29

---
+++ 1501-2000/leetcode1614.java
@@ -0,0 +1,21 @@
+
+DATE : 29-OCT-2025
+LEVEL : Easy
+TOPIC : String
+
+class Solution {
+    public int maxDepth(String s) {
+        int currentCount=0;
+        int maxCount=0;
+        for(char ch :s.toCharArray()){
+            if(ch=='('){
+                currentCount++;
+                 maxCount=Math.max(maxCount,currentCount);
+            }else if(ch==')'){
+                currentCount--;
+
+            }
+        }
+        return maxCount;
+    }
+}
