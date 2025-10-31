9994f83255420395c1b8396d83a364d7a573a5fb
Ranjitha2710 <ranjithasangamss@gmail.com>
2025-10-31 14:46:20 +0530

    oct 31

---
+++ 501-1000/leetcode876.java
@@ -0,0 +1,16 @@
+
+DATE : 31-OCT-2025
+LEVEL : Easy
+TOPIC : Linked List
+
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+        }
+        return slow;
+    }
+}
