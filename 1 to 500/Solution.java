9113e5219ae8e3cc89181005d6f919cb3438208f
Ranjitha2710 <ranjithasangamss@gmail.com>
2025-11-04 14:52:08 +0530

    nov 4

---
+++ 1 to 500/Solution.java
@@ -0,0 +1,20 @@
+
+DATE : 04-NOV-2025
+LEVEL : Easy
+TOPIC : Linked List(2pointer)
+
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast){
+                return true;
+            }
+        }
+        return false;
+        
+    }
+}
