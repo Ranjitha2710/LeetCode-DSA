2612011bb709484bc22432a7b8952eeba67a916c
Ranjitha2710 <ranjithasangamss@gmail.com>
2025-11-05 14:21:12 +0530

    nov 05

---
+++ 1 to 500/leetcode142.java
@@ -0,0 +1,24 @@
+
+DATE : 05-NOV-2025
+LEVEL : Medium
+TOPIC : Linked List(2-Pointer Approach)
+
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast!=null && fast.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
+            if(slow==fast){
+                slow=head;
+                while(slow!=fast){
+                    slow=slow.next;
+                    fast=fast.next;
+                }
+                return slow;
+            }
+        }
+        return null;
+    }
+}
