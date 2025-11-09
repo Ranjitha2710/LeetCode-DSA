282e34c22b455ef9af15136b76e8e43af3450b8e
Ranjitha2710 <ranjithasangamss@gmail.com>
2025-11-09 19:34:52 +0530

    Nov 9

---
+++ 1 to 500/leetcode4.java
@@ -0,0 +1,42 @@
+
+DATE : 09-NOV-2025
+LEVEL : Hard
+TOPIC : Binary search
+
+class Solution {
+
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+                if (nums1.length>nums2.length) {
+                    return findMedianSortedArrays(nums2, nums1);   
+        }
+        int n1=nums1.length;
+        int n2=nums2.length;
+        int low=0;
+        int high=n1;
+
+        while (low<=high) {
+            int cut1=(low+high)/2;
+            int cut2=(n1+n2+1)/2-cut1;
+        
+        int maxLeft1=(cut1==0)?Integer.MIN_VALUE:nums1[cut1-1];
+        int maxLeft2=(cut2==0)?Integer.MIN_VALUE:nums2[cut2-1];
+        int minRight1=(cut1==n1)?Integer.MAX_VALUE:nums1[cut1];
+        int minRight2=(cut2==n2)?Integer.MAX_VALUE:nums2[cut2];
+
+            if (maxLeft1<=minRight2 && maxLeft2<=minRight1) {
+                if ((n1+n2)%2 == 0) {
+                    return(Math.max(maxLeft1,maxLeft2)+Math.min(minRight1,minRight2))/2.0;    
+                }else{
+                    return Math.max(maxLeft1, maxLeft2);
+                } 
+            }
+            else if(maxLeft1>minRight2){
+                high=cut1-1;
+            }else{
+                low=cut1+1;
+            }
+        }
+        return 0.0;
+    }
+        
+    }
