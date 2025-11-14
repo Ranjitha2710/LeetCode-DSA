6579ea30633e82be76482d61b1666c46160af876
Ranjitha2710 <ranjithasangamss@gmail.com>
2025-11-14 15:58:33 +0530

    nov 14

---
+++ 1 to 500/leetcode392.java
@@ -0,0 +1,18 @@
+
+DATE : 14-NOV-2025
+LEVEL : Easy
+TOPIC : Strings
+
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int i=0;
+        int j=0;
+        while(i<s.length() && j<t.length()){
+            if(s.charAt(i)==t.charAt(j)){
+                i++;
+            }
+            j++;
+        }
+        return i==s.length();
+    }
+}
