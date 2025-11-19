f508783c3cd8482f8d11532e3be8b2d6e7a0004f
Ranjitha2710 <ranjithasangamss@gmail.com>
2025-11-19 10:14:01 +0530

    oct 10

--- 1 to 500/leetcode238.java
+++ 1 to 500/leetcode238.java
@@ -8,10 +8,12 @@ class Solution {
         int n=nums.length;
         int ans[]=new int[n];
         ans[0]=1;
-
+//left product of num except itself
         for(int i=1;i<n;i++){
             ans[i]=ans[i-1]*nums[i-1];
         }
+    //multipy right prod with left prod, 
+    // means include right side element fr product
         int rightProduct=1;
         for(int i=n-1;i>=0;i--){
             ans[i]*=rightProduct;
